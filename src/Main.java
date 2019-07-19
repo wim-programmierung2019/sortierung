151e61eced92e457814d4cf0172db44e133d8fdb
Christoph Rieger <rieger.chris@googlemail.com>
2019-07-19 22:38:28 +0000

    Prepare test support

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import org.junit.internal.TextListener;
+import org.junit.runner.JUnitCore;
+
 public class Main {
     
     /**
@@ -6,6 +9,14 @@ public class Main {
     public static void main(String[] args){
         // Implementieren Sie einen Algorithmus für Bubblesort in Java
         
+        
+        
+        
+        
+        
+        //JUnitCore junit = new JUnitCore();
+        //junit.addListener(new TextListener(System.out));
+        //junit.run(SortierungTest.class);
     }
     
 }

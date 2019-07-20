dd09a6f522abb60a8e08c99eb3801599265b4971
friej <julian-frie@gmx.de>
2019-07-20 08:06:35 +0000

    bubbleSort

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,35 @@ public class Main {
      */
     public static void main(String[] args){
         // Implementieren Sie einen Algorithmus für Bubblesort in Java
-        
-        
-        
-        
-        
+        /**Main bsort = new Main(); 
+        int[] sortList = bsort.sort();*/
+        int[] list={5,4,1,9,6,8};
+        int[] sortList=sort(list);
+        for(int i=0;i<list.length;i++){
+        System.out.print(list[i]);
+        } 
+    }
+
+    public static int[] sort(int [] zusortieren){
+        int k; //hilfsvariable
+        for(int i =1; i<zusortieren.length;i++){
+            for(int j =0; j<zusortieren.length-1;j++){
+            if(zusortieren[j]>zusortieren[j+1]){
+                k=zusortieren[j];
+                zusortieren[j]=zusortieren[j+1];
+                zusortieren[j+1]=k;
+            }else{
+                continue;
+                }
+            }
+        }
+        return zusortieren;
+           
         
         //JUnitCore junit = new JUnitCore();
         //junit.addListener(new TextListener(System.out));
         //junit.run(SortierungTest.class);
-    }
+  }
+ }
     
-}
+

7de32e7e6d684cc08c7f65cf836ee3a1ef3343ff
stojo <johannesstorck@web.de>
2019-07-20 08:03:24 +0000

    finished

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.awt.peer.SystemTrayPeer;
+
 import org.junit.internal.TextListener;
 import org.junit.runner.JUnitCore;
 
@@ -6,17 +8,40 @@ public class Main {
     /**
      * Sortierung
      */
-    public static void main(String[] args){
-        // Implementieren Sie einen Algorithmus für Bubblesort in Java
-        
-        
-        
+   public static void main(String[] args){
+	        // Implementieren Sie einen Algorithmus für Bubblesort in Java
+	        
+	        
+	        int list[] = {5,4,1,9,6,8};
+	        for(;;){
+	            boolean b = true;
+	            for(int i=0; i<list.length-1;i++){
+	                if(list[i]>list[i+1]){
+	                    b=false;
+	                    int temp = list[i+1];
+	                    list[i+1]=list[i];
+	                    list[i]=temp;
+	                }
+	            
+	            }
+	            if(b==true){
+	                for(int i=0; i<list.length;i++){
+	                    System.out.print(list[i]);
+	                }  
+	                break;
+	            }
+	        }
+	    }
+}
+
+
+
+
+ 
         
         
         
         //JUnitCore junit = new JUnitCore();
         //junit.addListener(new TextListener(System.out));
         //junit.run(SortierungTest.class);
-    }
-    
-}
+
